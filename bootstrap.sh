4272b18d09544e33778f83113ca6f44f91d031b0
Josh Charles <josh.charles@gmail.com>
2016-11-24 11:03:53 -0800

    removing vagrant file and renaming install to bootstrap to better reflect usage

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,3 @@ sudo apt-get install software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install ansible
-
-rm /etc/ansible/hosts
-touch /etc/ansible/hosts
-echo "localhost ansible_connection=local" > /etc/ansible/hosts
