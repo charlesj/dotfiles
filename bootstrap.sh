826b602bd49bee399fb2390a0a1e666daab1460d
Josh Charles <josh.charles@gmail.com>
2016-11-26 11:30:37 -0800

    Assume yes in bootstrapping.  Reminder to setup reminna

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt-get install software-properties-common
+sudo apt-get install -y software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
-sudo apt-get install ansible
+sudo apt-get install -y ansible
