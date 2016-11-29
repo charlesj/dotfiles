b18287abd8296eb0a8f9c8a509b8e3db489cd22b
Josh Charles <josh.charles@gmail.com>
2016-11-28 23:19:50 -0800

    Assume yes when adding ansbile repository in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sudo apt-get install -y software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
+sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
 sudo apt-get install -y ansible
